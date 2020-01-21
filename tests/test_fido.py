ea072c250b419e7a2f4f111ea2982a7d340d9a94
Aleksi Salo <aleksi.salo@csc.fi>
2020-01-21 12:53:03 +0200

    KDKPAS-2265 Timer test counts CPU time

--- tests/test_fido.py
+++ tests/test_fido.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from time import sleep
-
 from fido.fido import PerfTimer
 
 
 def test_perf_timer():
     timer = PerfTimer()
-    sleep(3.6)
+    x = 1000000
+    while x:
+        x -= 1
     duration = timer.duration()
     assert duration > 0
